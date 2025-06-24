0c9b9f8d60d6d9092f4c4ddf2b1c8196a4afaad3
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-06-24 08:26:59 +0000

    ok now I want to change the font of every heading in all pages get the I

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Roboto', 'sans-serif'],
-        headline: ['Cinzel', 'serif'],
+        headline: ['Montserrat', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
