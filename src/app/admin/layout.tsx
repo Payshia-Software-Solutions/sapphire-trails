c9d82d92d29f319a704f0894da7cf440af961870
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-06-25 10:58:41 +0000

    in this shen I scroll the right side side bar not scroll  fix that as we

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -31,8 +31,8 @@ export default function AdminLayout({
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <AdminSidebar />
-      <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-background px-4 lg:h-[60px] lg:px-6">
+      <div className="flex flex-col overflow-hidden">
+        <header className="flex h-14 shrink-0 items-center gap-4 border-b bg-background px-4 lg:h-[60px] lg:px-6">
             <Sheet>
                 <SheetTrigger asChild>
                     <Button
@@ -82,7 +82,7 @@ export default function AdminLayout({
                 {/* Can add search or breadcrumbs here */}
             </div>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background-alt">
+        <main className="flex-1 overflow-y-auto bg-background-alt p-4 lg:p-6">
           {children}
         </main>
       </div>
