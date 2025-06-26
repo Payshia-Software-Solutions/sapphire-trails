7efe8ef4b5bd2f13ad06f2af936ba47f3c653e63
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-06-26 07:41:23 +0000

    change the pre loader just like this image shows with animation current

--- src/components/shared/pre-loader.tsx
+++ src/components/shared/pre-loader.tsx
@@ -4,18 +4,18 @@ const GemIcon = () => (
     <svg
       width="80"
       height="80"
-      viewBox="0 0 24 24"
-      fill="currentColor"
+      viewBox="-100 -100 200 200"
       xmlns="http://www.w3.org/2000/svg"
-      className="text-primary animate-gem-pulse"
+      className="animate-gem-pulse"
     >
-      <path
-        d="M12 2L4.69259 8.24009C4.25268 8.60155 4 9.15548 4 9.73991V14.2601C4 14.8445 4.25268 15.3985 4.69259 15.7599L12 22L19.3074 15.7599C19.7473 15.3985 20 14.8445 20 14.2601V9.73991C20 9.15548 19.7473 8.60155 19.3074 8.24009L12 2Z"
-        stroke="hsl(var(--primary-foreground))"
-        strokeWidth="0.5"
-      />
-      <path d="M4 9.5L12 15L20 9.5" stroke="hsl(var(--primary-foreground))" strokeOpacity="0.5" strokeWidth="0.5" />
-      <path d="M12 22V15" stroke="hsl(var(--primary-foreground))" strokeOpacity="0.5" strokeWidth="0.5" />
+        <g>
+            <path d="M 0 -80 L 80 0 L 0 80 L -80 0 Z" fill="#4dd0e1"/>
+            <path d="M 0 -80 L -80 0 L -60 -10 L 0 -60 Z" fill="#26c6da"/>
+            <path d="M 0 -80 L 80 0 L 60 -10 L 0 -60 Z" fill="#00bcd4"/>
+            <path d="M 0 80 L -80 0 L -60 10 L 0 60 Z" fill="#00acc1"/>
+            <path d="M 0 80 L 80 0 L 60 10 L 0 60 Z" fill="#0097a7"/>
+            <path d="M 0 -60 L 60 -10 L 0 60 L -60 10 Z" fill="#80deea"/>
+        </g>
     </svg>
 );
 
