0c33bf75371ee516f529ed6c684040dad4d66745
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-06-26 10:14:17 +0000

    update the favicon from this https://favicon.io/ use this tp create fav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,15 @@ const cinzel = Cinzel({
 export const metadata: Metadata = {
   title: 'Sapphire Trails',
   description: "Sri Lanka's Only Luxury Gem Experience",
+  icons: {
+    apple: "/apple-touch-icon.png",
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    shortcut: "/favicon.ico",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
