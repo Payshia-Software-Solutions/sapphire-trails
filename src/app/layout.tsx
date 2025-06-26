b9a7a52e26fe5a53e02e6ffca4de56baebd16470
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-06-26 10:15:52 +0000

    still not shows the favicon bro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,12 +30,8 @@ export const metadata: Metadata = {
   title: 'Sapphire Trails',
   description: "Sri Lanka's Only Luxury Gem Experience",
   icons: {
-    apple: "/apple-touch-icon.png",
-    icon: [
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-    shortcut: "/favicon.ico",
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
   },
   manifest: "/site.webmanifest",
 };
