116a63af13ed6e18348ad3d8a04ae8aaf596895a
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-07-17 12:38:23 +0000

    WHY IS THIS ERROR IN WEB.PHP, <?php // Set CORS headers for every respon

--- var/www/html/routes/sitecontentRoutes.php
+++ var/www/html/routes/sitecontentRoutes.php
@@ -1,15 +1,15 @@
 <?php
 require_once './controllers/SiteContentController.php';
 
-return function ($router) {
-    $controller = new SiteContentController($GLOBALS['pdo']);
+$pdo = $GLOBALS['pdo'];
+$controller = new SiteContentController($pdo);
 
-    $router->get('/{section_key}', function($section_key) use ($controller) {
+return [
+    'GET /content/{section_key}/' => function ($section_key) use ($controller) {
         $controller->getSection($section_key);
-    });
-    
-    $router->post('/{section_key}', function($section_key) use ($controller) {
+    },
+    'POST /content/{section_key}/' => function ($section_key) use ($controller) {
         $controller->updateSection($section_key);
-    });
-};
+    },
+];
 ?>
