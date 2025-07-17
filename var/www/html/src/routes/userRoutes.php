8796f17e721a245ade9de6417be1c50dfa45a6b6
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-07-17 12:58:33 +0000

    now rhis has error why is that, admin user loading not working and loggi

---
+++ var/www/html/src/routes/userRoutes.php
@@ -0,0 +1,22 @@
+<?php
+require_once './controllers/UserController.php';
+
+return function ($router) {
+    $controller = new UserController($GLOBALS['pdo']);
+
+    // Route to get all users
+    $router->get('/', [$controller, 'getAll']);
+
+    // Route to get a specific user by ID
+    $router->get('/{id}', [$controller, 'getById']);
+    
+    // Route to get users by type (e.g., 'admin' or 'client')
+    $router->get('/type/{type}', [$controller, 'getByType']);
+
+    // Route to create a new user
+    $router->post('/', [$controller, 'create']);
+
+    // Route to delete a user
+    $router->delete('/{id}', [$controller, 'delete']);
+};
+?>
