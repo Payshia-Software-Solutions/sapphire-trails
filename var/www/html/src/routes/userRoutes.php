21cb77bce7f66083df0d63ab7c31061ed00bab2e
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-07-18 09:14:53 +0000

    when I add that you give code in above logging function isn't working so

--- var/www/html/src/routes/userRoutes.php
+++ var/www/html/src/routes/userRoutes.php
@@ -7,14 +7,17 @@ return function ($router) {
     // Route to get all users
     $router->get('/', [$controller, 'getAll']);
 
-    // Route to get a specific user by ID
-    $router->get('/{id}', [$controller, 'getById']);
+    // Route to create a new user (for signup)
+    $router->post('/', [$controller, 'create']);
     
+    // Route for login
+    $router->post('/login', [$controller, 'login']);
+
     // Route to get users by type (e.g., 'admin' or 'client')
     $router->get('/type/{type}', [$controller, 'getByType']);
 
-    // Route to create a new user
-    $router->post('/', [$controller, 'create']);
+    // Route to get a specific user by ID
+    $router->get('/{id}', [$controller, 'getById']);
 
     // Route to delete a user
     $router->delete('/{id}', [$controller, 'delete']);
